0388cef483e4ee62fc4f6e8397cdef581391bae7
Miika Holmlund <miika.holmlund@gmail.com>
2026-08-31 12:20:56 +0300

    make date handling always choose the next month

--- main.py
+++ main.py
@@ -42,8 +42,7 @@ time = os.environ["HOAS_TIME"]
 weekday = int(os.environ["HOAS_WEEKDAY"])
 machine_number = os.environ["HOAS_MACHINE"]
 
-start = datetime.datetime.today() + relativedelta(months=1, days=4)
-start = start.replace(day=1)
+start = datetime.datetime.today().replace(day=1) + relativedelta(months=1)
 days = []
 current = start
 while current.month == start.month:
